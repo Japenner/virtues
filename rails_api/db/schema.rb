689dce46c79bda984ce44ed8b7ea3880ead2c6de
Jacob Penner <japenner@gmail.com>
2024-02-05 07:33:57 -0500

    update database configurations

--- rails_api/db/schema.rb
+++ rails_api/db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_02_02_172057) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "journal_entries", force: :cascade do |t|
     t.integer "user_id"
     t.integer "virtue_id"
